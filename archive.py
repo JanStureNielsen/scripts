8cfa85c9f4483315945f541018e4cecc9cc7925d
Jussi Virtanen <jussi.k.virtanen@gmail.com>
2016-12-10 22:16:53 +0200

    Initial commit

---
+++ archive.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os.path
+import sys
+import zipfile
+
+if len(sys.argv) != 2:
+    sys.exit('Usage: {} <filename>'.format(os.path.basename(sys.argv[0])))
+
+filename = sys.argv[1]
+
+filenames = [line.strip() for line in sys.stdin]
+
+prefix, _ = os.path.splitext(filename)
+
+with zipfile.ZipFile(filename, 'w', zipfile.ZIP_DEFLATED) as outfile:
+    for filename in filenames:
+        outfile.write(filename, os.path.join(prefix, filename))
